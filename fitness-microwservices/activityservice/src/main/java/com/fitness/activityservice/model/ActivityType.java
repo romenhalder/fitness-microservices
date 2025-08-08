daada0a0681885c732b742b1be79e883ae297292
Romen Halder <105607137+romenhalder@users.noreply.github.com>
2025-08-09 01:16:35 +0530

    ai recommandation added

---
+++ fitness-microwservices/activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRANING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
